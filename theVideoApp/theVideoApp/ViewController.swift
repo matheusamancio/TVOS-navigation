05cda22c03a92fe91d8e47ceaa98c3a3c583e52e
Matheus Amancio Seixeiro <matheusamancio23@hotmail.com>
2016-05-04 22:21:08 -0300

    start video app

---
+++ theVideoApp/theVideoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  theVideoApp
+//
+//  Created by Matheus Amancio Seixeiro on 5/4/16.
+//  Copyright © 2016 Matheus Amancio Seixeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
